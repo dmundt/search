ecbd1a38d1003ef5304997fd6a2cb42395ad4055
Daniel Schubert <d.schubert@aboutnoise.com>
2012-10-24 10:06:54 +0200

    Add regex for file name and line to match.

--- bin/search.dart
+++ bin/search.dart
@@ -5,22 +5,22 @@
 // --path "d:/p4/omega/Baugruppen/VPU3400/Software/branches/unified-refactoring/apps/rtools/"
 // --path "d:/test/"
 
+//const _PARSE_FILE_REGEX = const RegExp(r'(\.h|\.cpp)$');
+//const _PARSE_COMMENT_REGEX = const RegExp(r'\w*\s*//\s*[a-z]+');
+
 library comments;
 
 import 'package:args/args.dart';
 import 'dart:io';
 
-const _PARSE_FILE_REGEX = const RegExp(r'(\.h|\.cpp)$');
-const _PARSE_COMMENT_REGEX = const RegExp(r'\w*\s*//\s*[a-z]+');
-
-Future<List<String>> _filterLine(lines) {
+Future<List<String>> _filterLine(lines, pattern) {
   var completer = new Completer<List<String>>();
   var number = 0;
   var result = <String>[];
   var line;
   while ((line = lines.readLine()) != null) {
     ++number;
-    if (line.contains(_PARSE_COMMENT_REGEX)) {
+    if (line.contains(pattern)) {
       result.add("${number}: $line");
     }
   }
@@ -28,8 +28,8 @@ Future<List<String>> _filterLine(lines) {
   return completer.future;
 }
 
-void _proccesLines(name, lines) {
-  _filterLine(lines).then((comments) {
+void _proccesLines(name, lines, pattern) {
+  _filterLine(lines, pattern).then((comments) {
     if (comments.length > 0) {
       print("File: $name");
       for (final x in comments) {
@@ -40,12 +40,12 @@ void _proccesLines(name, lines) {
   });
 }
 
-void _processFile(name) {
-  if (name.contains(_PARSE_FILE_REGEX)) {
+void _processFile(name, file, pattern) {
+  if (name.contains(file)) {
     var stream = new File(name).openInputStream();
     var lines = new StringInputStream(stream);
     lines.onLine = () {
-      _proccesLines(name, lines);
+      _proccesLines(name, lines, pattern);
     };
     lines.onClosed = () {
       stream.close();
@@ -53,12 +53,12 @@ void _processFile(name) {
   }
 }
 
-void _processFiles(String rootDir) {
+void _processFiles(String rootDir, RegExp file, RegExp pattern) {
   var path = new Path.fromNative(rootDir);
   var dir = new Directory.fromPath(path.directoryPath);
   var lister = dir.list(recursive:true);
   lister.onFile = (name) {
-    _processFile(name);
+    _processFile(name, file, pattern);
   };
 }
 
@@ -66,8 +66,12 @@ ArgParser get _pubArgParser {
   var parser = new ArgParser();
   parser.addFlag('help', abbr: 'h', negatable: false,
     help: 'Prints this usage information');
+  parser.addOption('file', abbr: 'f',
+      help: 'Sets the regular expression for file names to match');
   parser.addOption('path', abbr: 'p',
     help: 'Sets the current path to parse for invalid comments');
+  parser.addOption('regex', abbr: 'r',
+      help: 'Sets the regular expression for lines in each file to match');
   return parser;
 }
 
@@ -79,7 +83,17 @@ void main() {
     return;
   }
   if (globalOptions['path'] != null) {
-    _processFiles(globalOptions['path']);
+    // By default match every line.
+    var pattern = new RegExp('');
+    if (globalOptions['regex'] != null) {
+      pattern = new RegExp(globalOptions['regex']);
+    }
+    // By default match every file.
+    var file =  new RegExp('');
+    if (globalOptions['file'] != null) {
+      file = new RegExp(globalOptions['file']);
+    }
+    _processFiles(globalOptions['path'], file, pattern);
     return;
   } else {
     print('Missing path argument.');
